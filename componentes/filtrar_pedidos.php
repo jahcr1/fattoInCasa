11f865ec14ea79e454f2a6011c8ef41e188fc9cd
jahcr1 <martineta_contreras@hotmail.com>
2025-07-16 04:26:28 -0300

    CREATE archivo que maneja logica para filtrar un pedido o servicio de catering segun su estado

---
+++ componentes/filtrar_pedidos.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once('conexion.php');
+
+if (isset($_SESSION['administrador'])) {
+  if (!empty($_POST['estado'])) {
+    $estado = mysqli_real_escape_string($conexion, $_POST['estado']);
+    $query = "SELECT * FROM pedidos WHERE estado = ?";
+    $stmt = $conexion->prepare($query);
+    $stmt->bind_param("s", $estado);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+
+    $_SESSION['pedidos_filtrados'] = $resultado->fetch_all(MYSQLI_ASSOC);
+    header("Location: ../panel.php?seccion=mostrar-pedidos");
+    exit();
+  } else {
+    $_SESSION['error'] = "Debe seleccionar un estado.";
+    header("Location: ../panel.php?seccion=mostrar-pedidos");
+    exit();
+  }
+} else {
+  header("Location: ../panel.php?error=acceso-denegado");
+  exit();
+}
