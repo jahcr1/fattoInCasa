99cf8dadcfb0f32b3ad9c8ca766d1e03a5ad3b80
jahcr1 <martineta_contreras@hotmail.com>
2025-07-21 16:14:28 -0300

    UPD logica del archivo con nuevos campos y nueva consulta SQL actualizada

--- componentes/cargar_evento.php
+++ componentes/cargar_evento.php
@@ -4,7 +4,7 @@ require_once('conexion.php');
 
 $nombre = $_POST['nombre'];
 $entradas = $_POST['entradas'];
-$platos_principales = $_POST['plato_principales'];
+$platos_principales = $_POST['platos_principales'];
 $descripcion = $_POST['descripcion'];
 $estado = $_POST['estado'] ?? 'Disponible';
 
@@ -13,15 +13,15 @@ $imagen = $_FILES['imagen']['tmp_name'];
 $formato = $_FILES['imagen']['type'];
 $img_data = addslashes(file_get_contents($imagen));
 
-$query = "INSERT INTO eventos (nombre, entradas, descripcion, ci_imagen_plato, formato_imagen, estado)
-          VALUES ('$nombre', '$ingredientes', '$descripcion', '$img_data', '$formato', '$estado')";
+$query = "INSERT INTO eventos (nombre, entradas, platos_principales, descripcion, ci_imagen_plato, formato_imagen, estado)
+          VALUES ('$nombre', '$entradas', '$platos_principales', '$descripcion', '$img_data', '$formato', '$estado')";
 
 if (mysqli_query($conexion, $query)) {
-    $_SESSION['mensaje'] = "Plato cargado correctamente.";
+    $_SESSION['mensaje'] = "Evento cargado correctamente.";
 } else {
-    $_SESSION['mensaje'] = "Error al guardar el plato.";
+    $_SESSION['mensaje'] = "Error al guardar el evento.";
 }
 
-header("Location: ../panel.php?seccion=cargar-menu");
+header("Location: ../panel.php?seccion=cargar-evento");
 exit;
 ?>
